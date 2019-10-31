e4eeb851c7fc903d4b02796c3aea0bb69f43c3e2
Eoan O'Dea <odeaeoan@gmail.com>
2019-10-31 18:27:09 +0000

    Add and view list of books from db

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,12 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Welcome</div>
+                <div class="card-header">About</div>
 
                 <div class="card-body">
-                    Welcome to MoBookStore <a href={{ route('admin.books.index') }}>Books</a>
-                    <br />
-                    Learn more <a href={{ route('about') }}>about us</a>
+                    About Page
                 </div>
             </div>
         </div>
